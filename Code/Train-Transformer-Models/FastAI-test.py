ec300054cebc5929a0831fde2a4167778557c8d8
KevinRoitero <kevin.roitero@outlook.com>
2020-10-20 12:58:37 +0200

    added code for transformers

---
+++ Code/Train-Transformer-Models/FastAI-test.py
@@ -0,0 +1,373 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from pathlib import Path 
+import os
+import torch
+import torch.optim as optim
+import random 
+
+# fastai
+from fastai import *
+from fastai.text import *
+from fastai.callbacks import *
+
+# transformers
+from transformers import PreTrainedModel, PreTrainedTokenizer, PretrainedConfig
+from transformers import BertForSequenceClassification, BertTokenizer, BertConfig
+from transformers import RobertaForSequenceClassification, RobertaTokenizer, RobertaConfig
+from transformers import XLNetForSequenceClassification, XLNetTokenizer, XLNetConfig
+from transformers import XLMForSequenceClassification, XLMTokenizer, XLMConfig
+from transformers import DistilBertForSequenceClassification, DistilBertTokenizer, DistilBertConfig
+from transformers import AutoTokenizer, AutoModel, AutoConfig, AutoModelForSequenceClassification
+
+MODEL_CLASSES = {
+    'bert': (BertForSequenceClassification, BertTokenizer, BertConfig),
+    'xlnet': (XLNetForSequenceClassification, XLNetTokenizer, XLNetConfig),
+    'xlm': (XLMForSequenceClassification, XLMTokenizer, XLMConfig),
+    'roberta': (RobertaForSequenceClassification, RobertaTokenizer, RobertaConfig),
+    'distilbert': (DistilBertForSequenceClassification, DistilBertTokenizer, DistilBertConfig),
+    'biobert' : (AutoModelForSequenceClassification,AutoTokenizer,AutoConfig)
+}
+
+seed = 42
+use_fp16 = False
+bs = 16
+#model_type = 'roberta'
+#pretrained_model_name = 'roberta-base'
+
+#model_type = 'biobert'
+#pretrained_model_name="monologg/biobert_v1.0_pubmed_pmc"
+
+#model_type = 'biobert'
+#pretrained_model_name="emilyalsentzer/Bio_ClinicalBERT"
+
+model_type = 'bert'
+pretrained_model_name='bert-base-cased'
+
+#model_type = 'distilbert'
+#pretrained_model_name = 'distilbert-base-uncased'
+
+#model_type = 'xlm'
+#pretrained_model_name = 'xlm-clm-enfr-1024'
+
+#model_type = 'xlnet'
+#pretrained_model_name = 'xlnet-base-cased'
+
+
+# In[2]:
+import fastai
+import transformers
+print('fastai version :', fastai.__version__)
+print('transformers version :', transformers.__version__)
+
+def rand_bin_array(perc, size):
+    arr = np.zeros(size)
+    arr[:(perc*size)]  = 1
+    np.random.shuffle(arr)
+    return arr
+
+
+# In[58]:
+
+DATA_ROOT = Path("../")
+train = pd.read_csv('../src/text-14-17.txt', sep="\t").head(1000)
+test =  pd.read_csv('../src/text400K.txt', sep="\t")[0:100]
+leaner_name_save = "./trained_model-10M-400-bert-base-cased"
+
+TEXT_COLUMN = 'certificate'
+Y_COLUMN = 'UCOD'
+
+print(train.shape,test.shape)
+
+
+categories = np.unique(train[Y_COLUMN])
+test = test[test[Y_COLUMN].isin(categories)]
+
+
+print(train.head())
+print(test.head())
+print()
+
+
+model_class, tokenizer_class, config_class = MODEL_CLASSES[model_type]
+
+
+# In[6]:
+
+
+def seed_all(seed_value):
+    random.seed(seed_value) # Python
+    np.random.seed(seed_value) # cpu vars
+    torch.manual_seed(seed_value) # cpu  vars
+    defaults.device = torch.device('cuda') # makes sure the gpu is used
+
+    if torch.cuda.is_available(): 
+        torch.cuda.manual_seed(seed_value)
+        torch.cuda.manual_seed_all(seed_value) # gpu vars
+        torch.backends.cudnn.deterministic = True  #needed
+        torch.backends.cudnn.benchmark = False
+
+
+# In[7]:
+
+
+seed_all(seed)
+
+
+# In[8]:
+
+
+class TransformersBaseTokenizer(BaseTokenizer):
+    """Wrapper around PreTrainedTokenizer to be compatible with fast.ai"""
+    def __init__(self, pretrained_tokenizer: PreTrainedTokenizer, model_type = 'bert', **kwargs):
+        self._pretrained_tokenizer = pretrained_tokenizer
+        self.max_seq_len = pretrained_tokenizer.max_len
+        self.model_type = model_type
+
+    def __call__(self, *args, **kwargs): 
+        return self
+
+    def tokenizer(self, t:str) -> List[str]:
+        """Limits the maximum sequence length and add the spesial tokens"""
+        CLS = self._pretrained_tokenizer.cls_token
+        SEP = self._pretrained_tokenizer.sep_token
+        if self.model_type in ['roberta']:
+            tokens = self._pretrained_tokenizer.tokenize(t, add_prefix_space=True)[:self.max_seq_len - 2]
+            tokens = [CLS] + tokens + [SEP]
+        #if self.model_type in ['bert']:
+        #    return ["[CLS]"] + self._pretrained_tokenizer.tokenize(t)[:self.max_seq_len - 2] + ["[SEP]"]
+        else:
+            tokens = self._pretrained_tokenizer.tokenize(t)[:self.max_seq_len - 2]
+            if self.model_type in ['xlnet']:
+                tokens = tokens + [SEP] +  [CLS]
+            else:
+                tokens = [CLS] + tokens + [SEP]
+        return tokens
+
+
+# In[9]:
+
+transformer_tokenizer = tokenizer_class.from_pretrained(pretrained_model_name)
+transformer_base_tokenizer = TransformersBaseTokenizer(pretrained_tokenizer = transformer_tokenizer, model_type = model_type)
+fastai_tokenizer = Tokenizer(tok_func = transformer_base_tokenizer, pre_rules=[], post_rules=[])
+
+
+# In[10]:
+
+
+class TransformersVocab(Vocab):
+    def __init__(self, tokenizer: PreTrainedTokenizer):
+        super(TransformersVocab, self).__init__(itos = [])
+        self.tokenizer = tokenizer
+    
+    def numericalize(self, t:Collection[str]) -> List[int]:
+        "Convert a list of tokens `t` to their ids."
+        return self.tokenizer.convert_tokens_to_ids(t)
+        #return self.tokenizer.encode(t)
+
+    def textify(self, nums:Collection[int], sep=' ') -> List[str]:
+        "Convert a list of `nums` to their tokens."
+        nums = np.array(nums).tolist()
+        return sep.join(self.tokenizer.convert_ids_to_tokens(nums)) if sep is not None else self.tokenizer.convert_ids_to_tokens(nums)
+    
+    def __getstate__(self):
+        return {'itos':self.itos, 'tokenizer':self.tokenizer}
+
+    def __setstate__(self, state:dict):
+        self.itos = state['itos']
+        self.tokenizer = state['tokenizer']
+        self.stoi = collections.defaultdict(int,{v:k for k,v in enumerate(self.itos)})
+
+
+# In[11]:
+
+transformer_vocab =  TransformersVocab(tokenizer = transformer_tokenizer)
+numericalize_processor = NumericalizeProcessor(vocab=transformer_vocab)
+tokenize_processor = TokenizeProcessor(tokenizer=fastai_tokenizer, include_bos=False, include_eos=False)
+transformer_processor = [tokenize_processor, numericalize_processor]
+
+
+# In[12]:
+
+pad_first = bool(model_type in ['xlnet'])
+pad_idx = transformer_tokenizer.pad_token_id
+
+
+# In[13]:
+
+
+tokens = transformer_tokenizer.tokenize('example of sentence')
+print(tokens)
+ids = transformer_tokenizer.convert_tokens_to_ids(tokens)
+print(ids)
+transformer_tokenizer.convert_ids_to_tokens(ids)
+
+
+# In[14]:
+
+
+databunch = (TextList.from_df(train, cols=TEXT_COLUMN, processor=transformer_processor)
+             #.split_by_rand_pct(0.1,seed=seed)
+             .split_none()
+             .label_from_df(cols= Y_COLUMN)
+             #.add_test(test)
+             .databunch(bs=bs, pad_first=pad_first, pad_idx=pad_idx))
+
+
+print('[CLS] token :', transformer_tokenizer.cls_token)
+print('[SEP] token :', transformer_tokenizer.sep_token)
+print('[PAD] token :', transformer_tokenizer.pad_token)
+databunch.show_batch()
+
+
+# In[16]:
+
+
+print('[CLS] id :', transformer_tokenizer.cls_token_id)
+print('[SEP] id :', transformer_tokenizer.sep_token_id)
+print('[PAD] id :', pad_idx)
+test_one_batch = databunch.one_batch()[0]
+print('Batch shape : ',test_one_batch.shape)
+print(test_one_batch)
+
+
+# In[17]:
+
+
+class CustomTransformerModel(nn.Module):
+    def __init__(self, transformer_model: PreTrainedModel):
+        super(CustomTransformerModel,self).__init__()
+        self.transformer = transformer_model
+        
+    def forward(self, input_ids, attention_mask=None):
+        
+        # attention_mask
+        # Mask to avoid performing attention on padding token indices.
+        # Mask values selected in ``[0, 1]``:
+        # ``1`` for tokens that are NOT MASKED, ``0`` for MASKED tokens.
+        attention_mask = (input_ids!=pad_idx).type(input_ids.type()) 
+        
+        logits = self.transformer(input_ids,
+                                  attention_mask = attention_mask)[0]   
+        return logits
+
+
+# In[62]:
+
+
+config = config_class.from_pretrained(pretrained_model_name)
+config.num_labels = len(categories)
+config.use_bfloat16 = use_fp16
+
+##
+config.do_lower_case = True
+config.max_seq_length = 256 
+config.train_batch_size = 16
+config.eval_batch_size = 16
+config.gradient_accumulation_steps = 1
+config.small = False
+config.max_steps = -1
+config.num_train_epochs = 4.0
+config.device = None
+print(config)
+#assert False
+
+
+# In[ ]:
+
+
+transformer_model = model_class.from_pretrained(pretrained_model_name, config = config)
+custom_transformer_model = CustomTransformerModel(transformer_model = transformer_model)
+
+#device
+#device = torch.device("cuda" if torch.cuda.is_available() and args.use_cuda else "cpu")
+#custom_transformer_model.to(device)
+
+
+# In[64]:
+
+
+from fastai.callbacks import *
+from transformers import AdamW
+from functools import partial
+
+CustomAdamW = partial(AdamW, correct_bias=False)
+
+learner = Learner(databunch, 
+                  custom_transformer_model, 
+                  opt_func = CustomAdamW, 
+                  metrics=[accuracy])
+
+
+# Show graph of learner stats and metrics after each epoch.
+#learner.callbacks.append(ShowGraph(learner))
+
+# Put learn in FP16 precision mode. --> Seems to not working
+if use_fp16: learner = learner.to_fp16()
+
+
+# In[65]:
+
+
+print(learner.model)
+print(learner.summary())
+
+#learner.fit_one_cycle(4, max_lr=3e-5)
+learner_mapping = {k:v for k,v in enumerate(learner.data.classes)}
+
+#learner.save(leaner_name_save, return_path=True)
+learner.load(leaner_name_save)
+
+
+def get_preds_and_labelsBEA(dfin):
+    learner.data.add_test(TextList.from_df(dfin, cols=TEXT_COLUMN, processor=transformer_processor))
+    test_preds, y = learner.get_preds(DatasetType.Test)
+    test_preds = test_preds.detach().cpu().numpy()
+    
+    #y = y.detach().cpu().numpy()
+    y = np.array(dfin[Y_COLUMN].tolist())
+    best_preds = np.argmax(test_preds, axis=1)
+    all_preds_ranked = np.argsort(-test_preds, axis=1)
+
+    fun = lambda t: learner_mapping[t]
+    vfunc = np.vectorize(fun)
+    all_preds_ranked = vfunc(all_preds_ranked)
+    best_preds = vfunc(best_preds)
+    return best_preds, all_preds_ranked, y
+
+# In[95]:
+def compute_accuracy(real_labels, best_preds, all_preds):
+    #acc_1 = (real_labels == best_preds).mean()
+    acc_1 = np.mean([1 if r in a[:1] else 0 for (r,a) in zip(real_labels, all_preds)]) if all_preds is not None else -1
+    acc_3 = np.mean([1 if r in a[:3] else 0 for (r,a) in zip(real_labels, all_preds)]) if all_preds is not None else -1
+    acc_5 = np.mean([1 if r in a[:5] else 0 for (r,a) in zip(real_labels, all_preds)]) if all_preds is not None else -1
+    acc_10 = np.mean([1 if r in a[:10] else 0 for (r,a) in zip(real_labels, all_preds)]) if all_preds is not None else -1
+    acc_at_toal = np.mean([1 if r in a else 0 for (r,a) in zip(real_labels, all_preds)]) if all_preds is not None else -1
+    return np.round(acc_1,6), np.round(acc_3,6), np.round(acc_5,6), np.round(acc_10,6), np.round(acc_at_toal,6)
+
+
+# In[100]:
+
+best_preds, all_preds_ranked, y = get_preds_and_labelsBEA(test)
+acc_1, acc_3, acc_5, acc_10, acc_at_total = compute_accuracy(y, best_preds, all_preds_ranked)
+print("="*80)
+print("========== ACCURACY ON TEST ==========")
+print("Accuracy@1: ", acc_1)
+print("Accuracy@3: ", acc_3)
+print("Accuracy@5: ", acc_5)
+print("Accuracy@10: ", acc_10)
+print("Accuracy@total: ", acc_at_total)
+print()
+print("len of test is", len(y))
+print("="*80)
+
+print("done.")
+
+
+
